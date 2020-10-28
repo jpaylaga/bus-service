bc4e75da07d835dbb7ee4e2d8a2edce8d09f4cc8
Joshua Paylaga <joshuapaylaga@gmail.com>
2020-10-28 09:02:31 +0800

    Inject BusStopRepositoryInterface

--- app/Http/Controllers/BusStopController.php
+++ app/Http/Controllers/BusStopController.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\BusStopRepositoryInterface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BusStopController extends Controller
 {
+    /**
+     * @var BusStopRepositoryInterface
+     */
+    protected $busStopRepository;
+
+    /**
+     * BusStopController constructor.
+     * @param BusStopRepositoryInterface $busStopRepository
+     */
+    public function __construct(BusStopRepositoryInterface $busStopRepository)
+    {
+        $this->busStopRepository = $busStopRepository;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,21 +29,17 @@ class BusStopController extends Controller
      */
     public function index()
     {
-        return response()->json([
-            'message' => 'In Controller',
-        ]);
+        return $this->busStopRepository->all();
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return JsonResponse
      */
-    public function show($id)
+    public function show(int $id)
     {
-        return response()->json([
-            'message' => 'In Controller with ID',
-        ]);
+        return $this->busStopRepository->get($id);
     }
 }
